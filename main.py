c2b2520b8a810c75503331f85708f7e8ad881b39
Mujeeb Cpy <mujeebcpy@gmail.com>
2025-05-23 15:40:11 +0200

    completed player movement

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        # pygame.surface.fill("white")
+        dt = clock.tick(60)/1000
+        updatable.update(dt)
+        for member in drawable:
+            member.draw(screen)
         pygame.display.flip()
+        
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
